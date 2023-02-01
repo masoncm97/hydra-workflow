503da88ecab93b3e62b0f2229c67db79956c914a
masoncm97 <masoncmathew@gmail.com>
2023-01-31 19:18:09 -0800

    initial commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import Hydra from 'hydra-synth';
+import movie from '../assets/IMG_3037.mp4';
+
+var hydra = new Hydra({
+  detectAudio: false
+})
+
+// Image
+
+// s0.initImage(image)
+// osc(6).modulate(src(s0),1).out(o0)
+
+// Video 
+
+// s0.initVideo(movie)
+// src(s0).modulate(noise(4)).out(o0)
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        index: './src/index.js',
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        static: './dist',
+    },
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "index.bundle.js",
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js)$/,
+                exclude: /node_modules/,
+                use: "babel-loader",
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.mp4$/,
+                use: [
+                    {
+                        loader: "file-loader",
+                        options: {
+                            name: "[name].[ext]",
+                            outputPath: "video"
+                        }
+                    }
+                ]
+            }
+        ],
+    },
+};
